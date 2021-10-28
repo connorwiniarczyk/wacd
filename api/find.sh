994fc4f5930e7e327cdbc277745ded7dbf82a79e
Connor <connorwiniarczyk@gmail.com>
2021-10-27 22:01:15 -0400

    wip

--- api/find.sh
+++ api/find.sh
@@ -5,4 +5,4 @@
 longitude=$1
 latitude=$2
 
-redis-cli --raw geosearch locations fromlonlat $longitude $latitude byradius 1000 km withdist asc 
+redis-cli --raw geosearch locations fromlonlat $longitude $latitude byradius 10 km withdist asc 
